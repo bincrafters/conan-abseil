be905e75adce8071be8716c308a6ce545bbad9de
Jerry W <jerrywiltse@gmail.com>
2017-10-03 11:52:05 -0400

    fix archs filter

--- build.py
+++ build.py
@@ -22,6 +22,6 @@ if __name__ == "__main__":
         os.environ["CONAN_UPLOAD"]="https://api.bintray.com/conan/{0}/public-conan".format(username)
         os.environ["CONAN_REMOTES"]="https://api.bintray.com/conan/conan-community/conan"
 
-    builder = ConanMultiPackager(args="--build missing", archs="x86_64")
+    builder = ConanMultiPackager(args="--build missing", archs=["x86_64"])
     builder.add_common_builds()    
     builder.run()
