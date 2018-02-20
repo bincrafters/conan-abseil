feed453cc9c8d5111153a9efa42433f8d7e88faa
Jerry W <jerrywiltse@gmail.com>
2018-02-20 14:43:51 -0500

    filter non libstdc++11 builds

--- build.py
+++ build.py
@@ -2,10 +2,14 @@
 # -*- coding: utf-8 -*-
 
 
+from bincrafters import build_shared
 from bincrafters import build_template_default
 
 if __name__ == "__main__":
 
-    builder = build_template_default.get_builder()
+    builder = build_template_default.get_builder(pure_c=False)
+    
+    if build_shared.get_os() == "Linux":
+        builder.builds = filter(lambda b: b.settings['compiler.libcxx'] == 'libstdc++11', builder.items)
 
     builder.run()
